a4a3e804d36608420e4685aca08addb31aee3a77
ahuiwork <42926594+ahuiwork@users.noreply.github.com>
2026-03-17 15:11:12 +0800

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# 确保挂载的数据库目录有权限
-if [ -d "/data" ]; then
-    chmod -R 777 /data
-fi
+# 给予权限
+chmod -R 777 /data
 
-# 启动 Nginx (后台运行)
-service nginx start
+# 启动 Nginx (使用 -g daemon off 让它在当前进程运行，防止被杀)
+nginx -g "daemon on;"
 
-# 启动 Python 后端 (作为主进程前台运行)
-# 这样容器的 PID 1 就是 Python，不会被系统杀掉
+# 启动 Python 后端 (这是容器的主进程)
+# 只要这个进程活着，容器就不会被杀掉
 cd /app/python && uv run uvicorn valuecell.server.api.app:app --host 0.0.0.0 --port 8000
